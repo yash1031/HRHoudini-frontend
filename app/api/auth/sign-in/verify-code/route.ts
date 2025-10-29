4fad7dd88b8b1478e832efbab7676a88de7e55ff
yash1031 <yash735131@gmail.com>
2025-10-30 01:12:10 +0530

    Include credentials as include in verify-code route

--- app/api/auth/sign-in/verify-code/route.ts
+++ app/api/auth/sign-in/verify-code/route.ts
@@ -34,7 +34,7 @@ export async function POST(req: Request) {
           code: code,
           session: session,
         }),
-        // credentials: 'include'
+        credentials: 'include'
       }
     );
 
