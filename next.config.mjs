747c53e4e757322f6ad2fbd3df6b8defe07ee4f6
yash1031 <yash735131@gmail.com>
2025-11-02 16:07:14 +0530

    Removed logs for production builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-// import { Amplify } from 'aws-amplify';
-// import amplifyConfig from './lib/amplify-config';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone', // added for ECR , App Runner deployment
@@ -11,6 +9,12 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+  },
+  compiler: {
+    // Automatically remove console.* statements in production
+    removeConsole: process.env.NODE_ENV === 'production',
+    // OR keep only errors and warnings:
+    // removeConsole: { exclude: ['error', 'warn'] },
   },
 }
 // Amplify.configure(amplifyConfig)
